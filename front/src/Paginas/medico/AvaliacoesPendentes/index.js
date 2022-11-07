73f1205544134f1d46512612934caa52e9980d72
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2022-11-07 17:08:48 -0300

    Update index.js

--- front/src/Paginas/medico/AvaliacoesPendentes/index.js
+++ front/src/Paginas/medico/AvaliacoesPendentes/index.js
@@ -2,41 +2,37 @@ import React, { useState, useEffect } from "react";
 import "./table.css";
 import { Button, Table, Form } from "react-bootstrap";
 import SolicitarExame from "../SolicitarExame";
-import Axios from "axios"
+import Axios from "axios";
+
+let array_nomes = [];
+let array_cpf = [];
 
 function AvalicacoesPendentes() {
-
   const [solicitar, setSolicitar] = useState("")
-
-  let array_nomes = []
-  let array_cpf = []
-
-  const [nome, setNome] = useState([])
-  const [cpf, setCpf] = useState([])
-
+  const [nome, setNome] = useState("")
   useEffect(() => {
     Axios.get('http://localhost:3000/medico/solicited')
     .then((res) =>{
-     /*  console.log(res.data) */
+        array_nomes = []
         for (let x = 0; x < res.data.length; x++) {
           array_nomes.push(res['data'][x]['nome'])
           array_cpf.push(res['data'][x]['cpf'])
         }
-        setNome(array_nomes)
-        setCpf(array_cpf)
-
-        let found = array_nomes.find(element => element === 'Thiago');
-        console.log(found)
+        console.log(array_nomes)
     })
 },[])
+const [nomex , setNomex] = useState("")
 
-
+const chamar_nome = () => {
+  let found = array_nomes.find(element => element === nomex);
+  setNome(found) 
+}
   return (
     <>
       {solicitar ? (<SolicitarExame />) : (<>
         <Form.Group className="w-25 form mb-3" controlId="formBasicEmail">
           <Form.Label>Buscar:</Form.Label>
-          <Form.Control className="buscar" type="text" placeholder="" />
+          <Form.Control className="buscar" placeholder="" onChange={(e) => setNomex(e.target.value)}/>
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
         <Table striped bordered hover>
@@ -49,16 +45,16 @@ function AvalicacoesPendentes() {
           </thead>
           <tbody>
             <tr>
-              <td>{found}</td>
+              <td>{nome}</td>
               <td>111.111.111-11</td>
               <td>
-                <Button variant="success" size="sm" onClick={setSolicitar}>
+                <Button variant="success" size="sm" onClick={chamar_nome}>
                   Solicitar
                 </Button>
               </td>
             </tr>
             <tr>
-              <td>{cpf}</td>
+              <td></td>
               <td>111.111.111-11</td>
               <td>
                 <Button variant="success" size="sm" onClick={setSolicitar}>
@@ -73,5 +69,4 @@ function AvalicacoesPendentes() {
     </>
   );
 }
-
 export default AvalicacoesPendentes;
